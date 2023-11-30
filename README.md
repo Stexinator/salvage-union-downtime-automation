f61ddd28ffc1c7bc25f431a5563c55f40d737e9a
Stexinator <hurricane90@gmx.de>
2023-11-30 15:50:17 +0100

    support uses

--- README.md
+++ README.md
@@ -7,6 +7,9 @@
 
 ## Changelog
 
+### 0.0.3
+- support uses
+
 ### 0.0.2
   - reduced spam from starting downtime
   - minor change of texts
--- src/scripts/restock.js
+++ src/scripts/restock.js
@@ -10,15 +10,31 @@ export default class SalvageUnionDowntimeAutomationRestock {
     }
 
     static async refillPilot(actor) {
+        this.refillAllItemsWithUses(actor)
         actor.update({ 'system.hp.value': actor.system.hp.max });
         actor.update({ 'system.ability-points.value': actor.system['ability-points'].max });
         
     }
 
     static async refillMech(actor) {
+        this.refillAllItemsWithUses(actor)
         actor.update({ 'system.sp.value': actor.system.sp.max });
         actor.update({ 'system.energy-points.value': actor.system['energy-points'].max });
         actor.update({ 'system.heat.value': 0 });
        
+    }
+
+    static async refillAllItemsWithUses(actor) {
+        let systems = actor?.system?.systems || [];
+        let modules = actor?.system?.modules || [];
+        let equipments = actor?.system?.equipment || [];
+        let abilities = actor?.system?.abilities || [];
+
+        let items = systems.concat(modules).concat(equipments).concat(abilities).filter(item =>(item.system.uses.max && item.system.uses.max != 0))
+
+        items.forEach(item => {
+            item.update({'system.uses.value': item.system.uses.max})
+        })
+
     }
 }
